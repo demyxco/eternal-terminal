e9d1d64ae98be225236816e113946d52844306da
Demyx <info@demyx.sh>
2019-10-27 23:55:55 -0700

    Convert indentations from tabs to spaces

--- demyx-entrypoint.sh
+++ demyx-entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 if [ ! -f /etc/ssh/ssh_host_rsa_key ]; then
-	ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+    ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 fi
 
 if [ -f /home/demyx/.ssh/ssh_host_rsa_key ]; then
-	cp /home/demyx/.ssh/ssh_host_rsa_key /etc/ssh
-	cp /home/demyx/.ssh/ssh_host_rsa_key.pub /etc/ssh
+    cp /home/demyx/.ssh/ssh_host_rsa_key /etc/ssh
+    cp /home/demyx/.ssh/ssh_host_rsa_key.pub /etc/ssh
 else
-	cp /etc/ssh/ssh_host_rsa_key /home/demyx/.ssh
-	cp /etc/ssh/ssh_host_rsa_key.pub /home/demyx/.ssh
+    cp /etc/ssh/ssh_host_rsa_key /home/demyx/.ssh
+    cp /etc/ssh/ssh_host_rsa_key.pub /home/demyx/.ssh
 fi
 
 chown -R demyx:demyx /home/demyx
